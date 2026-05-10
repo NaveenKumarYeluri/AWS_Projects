411b3abf06f218d3b8c5bcc63429a6e291d9b998
Naveen Kumar Yeluri <naveenk0513@zohomail.in>
2026-05-10 18:06:20 +0530

    Changing SQL files as per requirement

--- 2_EducationSystem/SQLQueries/SQL_Queries_Admin_Work.sql
+++ 2_EducationSystem/SQLQueries/SQL_Queries_Admin_Work.sql
@@ -9,11 +9,12 @@ ANALYZE aws_project.applicant;
 ANALYZE aws_project.institute;
 
 
--- Size of tables in mb, total rows
+-- Size: Size in MB (Effectively the number of 1MB blocks).
+-- tbl_rows: no of rows in respective table.
 SELECT
     schema AS table_schema,
     "table" AS table_name,
-    size AS size_in_mb,
+    size AS total_megabytes,
     tbl_rows AS total_rows
 FROM svv_table_info
 WHERE
@@ -24,8 +25,14 @@ ORDER BY size DESC;
 
 
 -- pct_used: Percentage of total storage used by this table
--- skew_rows: The ratio of the most populated slice to the least
+-- skew_rows: Ratio of rows on the most populated compute partition vs. the least populated.
+    -- 1.00 -> Perfect distribution
+    -- 1.20 -> Good distribution
+    -- 5.00 or higher -> This means DISTKEY is flawed, and one invisible compute node is doing 5x the work of the others, creating a massive bottleneck.
 -- diststyle: type of distribution key
+-- skew_sortkey1: Ratio of the size of the un-sorted region to the sorted region
+    -- 1.00 -> Perfectly sorted according to the SORT KEY.
+    -- If this number starts growing high, it means you have loaded a lot of new data and Redshift hasn't sorted it yet. We would run a VACUUM command to fix this, though Serverless often auto-vacuums in the background.
 SELECT
     database,
     schema AS table_schema,
@@ -35,6 +42,7 @@ SELECT
     diststyle,
     sortkey1,
     skew_rows,
+    skew_sortkey1,
     "size" as size_mb,
     pct_used
 FROM svv_table_info
@@ -45,9 +53,9 @@ ORDER BY skew_rows DESC;
 
 
 
--- stats_off: db statistics, since it is 0 stats are fine
--- unsorted: if we have any unsorted data, since 0 fine
--- empty: if we have empty blocks, since 0 fine
+-- stats_off: db statistics, since it is 0 stats are fine i.e., they are up-to-date
+-- unsorted: if we have any unsorted data or not, since 0 fine
+-- empty: if we have empty blocks or not, since 0 fine
 SELECT
     schema AS table_schema,
     "table" AS table_name,
@@ -79,6 +87,8 @@ ORDER BY tablename, "column";
 
 
 -- load history, you need to pass QueryId
+-- Query_id is must here unlike sys_query_history we cannot search with actual query we ran.
+-- It shows data_source, status of operation, and lot of other info related to the file operation.
 SELECT
     query_id,
     status,
@@ -88,3 +98,21 @@ SELECT
     end_time
 FROM sys_load_history
 ORDER BY start_time DESC;
+
+
+
+-- This shows info related to the Queries we run.
+-- Almost required info will be available, from who has run to it status, time taken at different stages etc.
+SELECT
+    query_id,
+    user_id,
+    query_text,
+    start_time,
+    end_time,
+    -- How long the query actually took
+    DATEDIFF(microsecond, start_time, end_time) / 1000000.0 AS duration_seconds,
+    *
+FROM sys_query_history
+WHERE query_text ILIKE '%COPY aws_project.applicant%'
+ORDER BY start_time DESC
+LIMIT 20;

c5ce70e2bfe3f986208b8d605e8e35899e2998f6
Naveen Kumar Yeluri <naveenk0513@zohomail.in>
2026-06-18 19:30:02 +0530

    including run times for todays runs

--- 3_Performance_Tuning/2_Order_Fulfillment/SQLFiles/redshift_queries_comparision.sql
+++ 3_Performance_Tuning/2_Order_Fulfillment/SQLFiles/redshift_queries_comparision.sql
@@ -18,6 +18,7 @@ GROUP BY
     mfg_facility_id
 ORDER BY
     avg_delivery_days ASC;--Took (15th June): 2m 21.3s, 19.1s, 4.7s, 4.8s, 4.7s
+-- Next Day (18th June): 10.4s, 7.5s, 6s, 6s, 6.3s
 
 
 -- --- OPTIMIZED VERSION ---
@@ -31,6 +32,7 @@ GROUP BY
     mfg_facility_id
 ORDER BY
     avg_delivery_days ASC;--Took (15th June): 49.9s, 11.9s, 3.9s, 3.4s, 3.5s
+-- Next Day (18th June): 8.4s, 6.7s, 5.3s, 5.4s, 5.4s
 
 
 
@@ -48,6 +50,7 @@ GROUP BY
     DATE(fc_release_to_mfg_date)
 ORDER BY
     daily_backlog DESC;--Took (15th June): 1m 47s, 1m 19.3s, 59s, 58.2s, 58.1s
+-- Next Day (18th June): 1m 21.7s, 1m 11.9s, 59.2s, 57.7s, 58s
 
 
 -- --- OPTIMIZED VERSION ---
@@ -63,6 +66,7 @@ GROUP BY
     DATE(fc_release_to_mfg_date)
 ORDER BY
     daily_backlog DESC;--Took (15th June): 2m 2.1s, 1m 25.9s, 1m 6.4s, 1m 6.7s, 1m 6.5s
+-- Next Day (18th June): 1m 34.7s, 1m 22.1s, 1m 6.1s, 1m 7s, 1m 6.7s
 
 
 
@@ -78,6 +82,7 @@ ORDER BY
     total_inspections DESC
 LIMIT
     1;--Took (15th June): 15260 ms, 168003 ms, 3706 ms, 3696 ms, 3687 ms
+-- Next Day (18th June): 4847 ms, 49 ms, 49 ms, 63 ms, 69 ms
 
 
 -- --- OPTIMIZED VERSION ---
@@ -91,6 +96,7 @@ ORDER BY
     total_inspections DESC
 LIMIT
     1;--Took (15th June): 15451 ms, 59332 ms, 10195 ms, 10119 ms, 10081 ms
+-- Next Day (18th June): 7099 ms, 60 ms, 48 ms, 48 ms, 49 ms
 
 
 
@@ -101,6 +107,7 @@ CREATE TABLE aws_project.temp_global_delay_def AS
 SELECT
     AVG(DATEDIFF(day, fc_release_to_mfg_date, to_mfg_facility_arrival_date)) AS avg_system_delay_days
 FROM aws_project.order_fulfillment_def;--Took (15th June): 13.3s, 4.1s, 4.3s, 4.3s, 4.3s
+-- Next Day (18th June): 5.1s, 3.7s, 3.7s, 4s, 3.6s
 
 
 -- --- OPTIMIZED VERSION ---
@@ -109,6 +116,7 @@ CREATE TABLE aws_project.temp_global_delay_opt AS
 SELECT
     AVG(DATEDIFF(day, fc_release_to_mfg_date, to_mfg_facility_arrival_date)) AS avg_system_delay_days
 FROM aws_project.order_fulfillment_opt;--Took (15th June): 7.5s, 4.7s, 4.5s, 4.6s, 4.6s
+-- Next Day (18th June): 5.1s, 4.6s, 4.5s, 4.6s, 4.5s
 
 
 
@@ -128,6 +136,7 @@ ORDER BY
     total_released DESC
 LIMIT
     1;--Took (15th June): 3060 ms, 2761 ms, 2564 ms, 2648 ms, 2569 ms
+-- Next Day (18th June): 3978 ms, 3668 ms, 3620 ms, 3579 ms, 3639 ms
 
 
 -- --- OPTIMIZED VERSION ---
@@ -145,6 +154,7 @@ ORDER BY
     total_released DESC
 LIMIT
     1;--Took (15th June): 616 ms, 451 ms, 313 ms, 320 ms, 312 ms
+-- Next Day (18th June): 1039 ms, 527 ms, 459 ms, 537 ms, 469 ms
 
 
 
@@ -159,6 +169,7 @@ SELECT
 FROM aws_project.order_fulfillment_def
 GROUP BY
     mfg_facility_id;--Took (15th June): 11.5s, 4.2s, 3.9s, 4.2s, 3.9s
+-- Next Day (18th June): 6.9s, 4.6s, 4.5s, 4.4s, 4.3s
 
 
 -- --- OPTIMIZED VERSION ---
@@ -171,6 +182,7 @@ SELECT
 FROM aws_project.order_fulfillment_opt
 GROUP BY
     mfg_facility_id;--Took (15th June): 11.3s, 4.2s, 3.7s, 3.5s, 3.7s
+-- Next Day (18th June): 6.7s, 4.8s, 4.8s, 4.7s, 4.8s
 
 
 
@@ -186,6 +198,7 @@ ORDER BY
     capability_score DESC
 LIMIT
     25;--Took (15th June): 4726 ms, 2062 ms, 1953 ms, 1964 ms, 1966 ms
+-- Next Day (18th June): 2739 ms, 68 ms, 47 ms, 49 ms, 58 ms
 
 
 -- --- OPTIMIZED VERSION ---
@@ -199,6 +212,7 @@ ORDER BY
     capability_score DESC
 LIMIT
     25;--Took (15th June): 4993 ms, 1845 ms, 1751 ms, 1749 ms, 1747 ms
+-- Next Day (18th June): 2416 ms, 57 ms, 59 ms, 49 ms, 55 ms
 
 
 
@@ -214,6 +228,7 @@ GROUP BY
     mfg_facility_product_capability_count
 ORDER BY
     capability_tier ASC;--Took (15th June): 2.6s, 1.9s, 1.8s, 1.7s, 1.8s
+-- Next Day (18th June): 4.3s, 1.8s, 1.8s, 1.7s, 1.8s
 
 
 -- --- OPTIMIZED VERSION ---
@@ -227,3 +242,4 @@ GROUP BY
     mfg_facility_product_capability_count
 ORDER BY
     capability_tier ASC;--Took (15th June): 2.6s, 1.9s, 1.7s, 1.8s, 2.1s
+-- Next Day (18th June): 4.1s, 2.3s, 2.3s, 2.4s, 2.4s
